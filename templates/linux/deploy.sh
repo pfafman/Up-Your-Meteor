647fca7a193131dc30148b33003d6a39849ebcaa
Timothy Pfafman <tim@pfafman.com>
2018-09-18 08:03:26 -0600

    double rm npm modules

--- templates/linux/deploy.sh
+++ templates/linux/deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+APP_NAME="<%= appName %>"
+CHECK_WAIT_TIME=<%= deployCheckWaitTime %> 
+
 # utilities
 gyp_rebuild_inside_node_modules () {
   for npmModule in ./*; do
@@ -64,8 +67,8 @@ revert_app (){
   if [[ -d old_app ]]; then
     sudo rm -rf app
     sudo mv old_app app
-    sudo service <%= appName %> stop || :
-    sudo service <%= appName %> start || :
+    sudo service $APP_NAME stop || :
+    sudo service $APP_NAME start || :
 
     echo "Latest deployment failed! Reverted back to the previous version." 1>&2
     exit 1
@@ -78,10 +81,11 @@ revert_app (){
 # logic
 set -e
 
-APP_DIR=/opt/<%= appName %>
+APP_DIR=/opt/$APP_NAME
 TMP_DIR=${APP_DIR}/tmp
 BUNDLE_DIR=${TMP_DIR}/bundle
 
+echo "cd ${TMP_DIR}"
 cd ${TMP_DIR}
 sudo rm -rf bundle
 sudo tar xvzf bundle.tar.gz > /dev/null
@@ -91,14 +95,19 @@ sudo chown -R ${USER} ${BUNDLE_DIR}
 # setting up NPMs
 cd ${BUNDLE_DIR}/programs/server
 echo "Reinstall npm modules"
+echo "Remove node_modules"
 sudo rm -rf node_modules
 
 sudo chown -R ${USER} ~/.node-gyp
 sudo chown -R ${USER} ~/.npm
 
+echo "Remove node_modules (again?)"
+sudo rm -rf node_modules
+
+echo "npm install"
 npm install
 
-cd /opt/<%= appName %>/
+cd /opt/$APP_NAME/
 
 # remove old app, if it exists
 if [ -d old_app ]; then
@@ -114,20 +123,20 @@ sudo mv tmp/bundle app
 
 # #wait and check
 # echo "Waiting for MongoDB to initialize. (5 minutes)"
-# . /opt/<%= appName %>/config/env.sh
+# . /opt/$APP_NAME/config/env.sh
 # wait-for-mongo ${MONGO_URL} 300000
 
 # restart app
-sudo service <%= appName %> stop || :
-sudo service <%= appName %> start || :
+sudo service $APP_NAME stop || :
+sudo service $APP_NAME start || :
 
-echo "Waiting for <%= deployCheckWaitTime %> seconds while app is booting up"
+echo "Waiting for $CHECK_WAIT_TIME seconds while app is booting up"
 
 COUNTER=0
 while [  $COUNTER -lt 10 ]; do
    echo The counter is $COUNTER
    let COUNTER=COUNTER+1
-   sleep <%= deployCheckWaitTime %>
+   sleep $CHECK_WAIT_TIME
    curl localhost:${PORT} && let COUNTER=10
 done
 
